43ce40bdfa35415ae424b92f40371f3e3bd2830c
Nemes Tamás - Mechwart <nemestamas94@gmail.com>
2024-03-19 16:36:12 +0100

    getAllRefuels api endpoint created

---
+++ refuels/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# http://localhost:8000/refuels/all/
+
+urlpatterns = [
+    path('all/', views.getAllRefuels)
+]
--- refuels/views.py
+++ refuels/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Refuel
+from .serializer import RefuelSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def getAllRefuels(request):
+    refuels = Refuel.objects.all().order_by('-date')
+    serialized = RefuelSerializer(refuels, many = True)
+    return Response(serialized.data)
